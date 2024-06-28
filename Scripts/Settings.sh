d65daa8bcb960e5ae54b8851deace876fb7b491a
zongheng23333 <1954640276@qq.com>
2024-06-28 13:40:52 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -36,5 +36,6 @@ echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
 #手动调整的插件
 if [ -n "$WRT_PACKAGE" ]; then
 	echo "$WRT_PACKAGE" >> ./.config
+ 	echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
 fi
 
